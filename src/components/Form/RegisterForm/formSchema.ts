ece75ed77526cb5d46ce4daf1f5b36d37ac2b7a6
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-06 17:10:01 -0300

    refactor: melhorar validação do yup

--- src/components/Form/RegisterForm/formSchema.ts
+++ src/components/Form/RegisterForm/formSchema.ts
@@ -1,6 +1,13 @@
 import * as yup from 'yup';
 
 export const formSchema = yup.object().shape({
+  name: yup.string().required('Preencha com seu nome.'),
   email: yup.string().required('Preencha com seu email.'),
-  password: yup.string().required('Preencha com sua senha.'),
+  password: yup
+    .string()
+    .required('Senha obrigatória.')
+    .min(8, 'Sua senha deve conter ao menos 8 caractéres.'),
+  confirmPass: yup
+    .string()
+    .oneOf([yup.ref('password')], 'As senhas precisam ser iguais.'),
 });
