ddaea0965ffc32e66f38eca65fbbb80dae8597a1
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-06 17:11:47 -0300

    feat: adicionar requisição post para cadastrar usuario.

--- src/components/Form/RegisterForm/index.tsx
+++ src/components/Form/RegisterForm/index.tsx
@@ -2,11 +2,14 @@ import { StyledTextField } from '../../../styles/form';
 import { StyledParagraph } from '../../../styles/typography';
 import { StyledButton } from '../../../styles/button';
 import { StyledForm } from '../../../styles/form';
+
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { iEntrieRegister } from '../../../contexts/types/types';
 import { formSchema } from './formSchema';
 import { useNavigate } from 'react-router-dom';
 import { yupResolver } from '@hookform/resolvers/yup';
+import api from '../../../contexts/axios/axiosInstance';
+import { showToast } from '../../../helpers/verifyToast';
 
 const RegisterForm = () => {
   const navigate = useNavigate();
@@ -18,35 +21,58 @@ const RegisterForm = () => {
     resolver: yupResolver(formSchema),
   });
 
-  <StyledForm>
-    <fieldset>
-      <StyledTextField label='Email' type='text' {...register('name')} />
-      <StyledParagraph fontColor='red'>{`${
-        errors.name?.message ?? ''
-      }`}</StyledParagraph>
-    </fieldset>
-    <fieldset>
-      <StyledTextField label='Email' type='text' {...register('email')} />
-      <StyledParagraph fontColor='red'>{`${
-        errors.email?.message ?? ''
-      }`}</StyledParagraph>
-    </fieldset>
-    <fieldset>
-      <StyledTextField label='Email' type='text' {...register('password')} />
-      <StyledParagraph fontColor='red'>{`${
-        errors.password?.message ?? ''
-      }`}</StyledParagraph>
-    </fieldset>
-    <fieldset>
-      <StyledTextField label='Email' type='text' {...register('confirmPass')} />
-      <StyledParagraph fontColor='red'>{`${
-        errors.confirmPass?.message ?? ''
-      }`}</StyledParagraph>
-    </fieldset>
-    <StyledButton $buttonSize='default' $buttonStyle='gray'>
-      Cadastrar
-    </StyledButton>
-  </StyledForm>;
+  const submitRegister: SubmitHandler<iEntrieRegister> = async (data) => {
+    try {
+      const response = await api.post('users', data);
+      showToast('Cadastro bem sucedido!', 'success');
+
+      setTimeout(() => {
+        navigate('/');
+      }, 2000);
+    } catch (error) {
+      showToast('Erro ao cadastrar.', 'error');
+    }
+  };
+
+  return (
+    <StyledForm onSubmit={handleSubmit(submitRegister)}>
+      <fieldset>
+        <StyledTextField label='Nome' type='text' {...register('name')} />
+        <StyledParagraph fontColor='red'>{`${
+          errors.name?.message ?? ''
+        }`}</StyledParagraph>
+      </fieldset>
+      <fieldset>
+        <StyledTextField label='Email' type='text' {...register('email')} />
+        <StyledParagraph fontColor='red'>{`${
+          errors.email?.message ?? ''
+        }`}</StyledParagraph>
+      </fieldset>
+      <fieldset>
+        <StyledTextField
+          label='Senha'
+          type='password'
+          {...register('password')}
+        />
+        <StyledParagraph fontColor='red'>{`${
+          errors.password?.message ?? ''
+        }`}</StyledParagraph>
+      </fieldset>
+      <fieldset>
+        <StyledTextField
+          label='Confirmar senha'
+          type='password'
+          {...register('confirmPass')}
+        />
+        <StyledParagraph fontColor='red'>{`${
+          errors.confirmPass?.message ?? ''
+        }`}</StyledParagraph>
+      </fieldset>
+      <StyledButton type='submit' $buttonSize='default' $buttonStyle='gray'>
+        Cadastrar
+      </StyledButton>
+    </StyledForm>
+  );
 };
 
 export default RegisterForm;
