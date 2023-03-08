0ef31062226a2cd53765813b5dfe2bb4766c9f68
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-07 22:01:58 -0300

    feat: adicionando produtos na ShopPage.

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,10 +1,48 @@
+import { useContext, useEffect, useState } from 'react';
+import api from '../../contexts/axios/axiosInstance';
+import { CartContext } from '../../contexts/CartContext';
 import ProductCard from './ProductCard';
 import { StyledProductList } from './style';
 
-const ProductList = () => (
-  <StyledProductList>
-    <ProductCard />
-  </StyledProductList>
-);
+const ProductList = () => {
+  const [products, setProducts] = useState<any[]>([]);
+  const [allProducts, setAllProducts] = useState<any[]>([]);
+  const { cart, setCart } = useContext(CartContext);
+
+  useEffect(() => {
+    const token = localStorage.getItem('@KenzieBurguer:token');
+    if (token) {
+      const catchProducts = async () => {
+        try {
+          const res = await api.get('products', {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          setProducts(res.data);
+          setAllProducts(res.data);
+        } catch (error) {}
+      };
+      catchProducts();
+    }
+  }, []);
+
+  return (
+    <StyledProductList>
+      {products.length > 0 &&
+        products.map((product) => (
+          <ProductCard
+            key={product.id.toString()}
+            name={product.name}
+            price={parseFloat(product.price.toString())
+              .toFixed(2)
+              .replace('.', ',')}
+            category={product.category}
+            img={product.img}
+          />
+        ))}
+    </StyledProductList>
+  );
+};
 
 export default ProductList;
