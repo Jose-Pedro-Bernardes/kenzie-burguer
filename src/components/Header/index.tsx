6bceac19641e4605fef9e1018a54221a3f54495e
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-08 11:53:37 -0300

    feat: adicionando logica de abrir modal e fazer logout no header.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,35 +5,38 @@ import { StyledHeader } from './style';
 import LogoKenzieBurguer from '../../assets/LogoKenzieBurguer.svg';
 
 import { StyledContainer } from '../../styles/grid';
+import { CartContext } from '../../contexts/CartContext';
+import { useContext } from 'react';
+import { UserContext } from '../../contexts/UserContext';
 
-const Header = () => (
-  <StyledHeader>
-    <StyledContainer containerWidth={1300}>
-      <div className='flexGrid'>
-        <img
-          src={LogoKenzieBurguer}
-          alt='Kenzie Burguer Logo'
-          className='logo'
-        />
-        <nav className='nav' role='navigation'>
-          <SearchForm />
-          <div className='buttons'>
-            <button
-              type='button'
-              onClick={() => {
-                console.log('Criar lógica');
-              }}
-            >
-              <MdShoppingCart size={28} />
-            </button>
-            <button type='button'>
-              <MdLogout size={28} />
-            </button>
-          </div>
-        </nav>
-      </div>
-    </StyledContainer>
-  </StyledHeader>
-);
+const Header = () => {
+  const { openModal } = useContext(CartContext);
+  const { userLogout } = useContext(UserContext);
+
+  return (
+    <StyledHeader>
+      <StyledContainer containerWidth={1300}>
+        <div className='flexGrid'>
+          <img
+            src={LogoKenzieBurguer}
+            alt='Kenzie Burguer Logo'
+            className='logo'
+          />
+          <nav className='nav' role='navigation'>
+            <SearchForm />
+            <div className='buttons'>
+              <button type='button' onClick={openModal}>
+                <MdShoppingCart size={28} />
+              </button>
+              <button onClick={userLogout} type='button'>
+                <MdLogout size={28} />
+              </button>
+            </div>
+          </nav>
+        </div>
+      </StyledContainer>
+    </StyledHeader>
+  );
+};
 
 export default Header;
