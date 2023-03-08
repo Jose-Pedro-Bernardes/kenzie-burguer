45dc872d8907b59c2e1d54b34a5972e18af4aec5
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-07 21:59:04 -0300

    feat: cart renderizando produtos.

--- src/components/CartModal/CartProductList/index.tsx
+++ src/components/CartModal/CartProductList/index.tsx
@@ -3,23 +3,35 @@ import CartProductCard from './CartProductCard';
 import { StyledCartProductList } from './style';
 import { StyledButton } from '../../../styles/button';
 import { StyledParagraph } from '../../../styles/typography';
+import { useCartContext } from '../../../hooks/useCartContext';
 
-const CartProductList = () => (
-  <StyledCartProductList>
-    <ul>
-      <CartProductCard />
-    </ul>
-
-    <div className='totalBox'>
-      <StyledParagraph>
-        <strong>Total</strong>
-      </StyledParagraph>
-      <StyledParagraph className='total'>R$ 14,00</StyledParagraph>
-    </div>
-    <StyledButton $buttonSize='default' $buttonStyle='gray'>
-      Remover todos
-    </StyledButton>
-  </StyledCartProductList>
-);
+const CartProductList = () => {
+  const { cart } = useCartContext();
+  return (
+    <StyledCartProductList>
+      <ul>
+        {cart &&
+          cart.map((item) => {
+            return (
+              <CartProductCard
+                key={item.id.toString()}
+                img={item.img}
+                name={item.name}
+              />
+            );
+          })}
+      </ul>
+      <div className='totalBox'>
+        <StyledParagraph>
+          <strong>Total</strong>
+        </StyledParagraph>
+        <StyledParagraph className='total'>R$ 14,00</StyledParagraph>
+      </div>
+      <StyledButton $buttonSize='default' $buttonStyle='gray'>
+        Remover todos
+      </StyledButton>
+    </StyledCartProductList>
+  );
+};
 
 export default CartProductList;
