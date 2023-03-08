239e20249a15519739abbbccdc3ba3406a7b7fef
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-07 21:57:15 -0300

    feat: botão de fechar modal funcionando.

--- src/components/CartModal/index.tsx
+++ src/components/CartModal/index.tsx
@@ -3,36 +3,44 @@ import CartProductList from './CartProductList';
 
 import { StyledCartModalBox } from './style';
 import { StyledParagraph, StyledTitle } from '../../styles/typography';
+import { useState } from 'react';
 
-const CartModal = () => (
-  <StyledCartModalBox>
-    <dialog>
-      <header>
-        <StyledTitle tag='h2' $fontSize='three'>
-          Carrinho de compras
-        </StyledTitle>
-        <button
-          type='button'
-          aria-label='Fechar'
-          onClick={() => {
-            console.log('Lógica aqui');
-          }}
-        >
-          <MdClose size={21} />
-        </button>
-      </header>
-      <div className='cartBox'>
-        <CartProductList />
+const CartModal = () => {
+  const [showModal, setShowModal] = useState(true);
 
-        <div className='emptyBox'>
-          <StyledTitle tag='h3' $fontSize='three' textAlign='center'>
-            Sua sacola está vazia
-          </StyledTitle>
-          <StyledParagraph textAlign='center'>Adicione itens</StyledParagraph>
-        </div>
-      </div>
-    </dialog>
-  </StyledCartModalBox>
-);
+  const closeModal = () => {
+    setShowModal(false);
+  };
+  return (
+    <>
+      {showModal && (
+        <StyledCartModalBox>
+          <dialog>
+            <header>
+              <StyledTitle tag='h2' $fontSize='three'>
+                Carrinho de compras
+              </StyledTitle>
+              <button type='button' aria-label='Fechar' onClick={closeModal}>
+                <MdClose size={21} />
+              </button>
+            </header>
+            <div className='cartBox'>
+              <CartProductList />
+
+              <div className='emptyBox'>
+                <StyledTitle tag='h3' $fontSize='three' textAlign='center'>
+                  Sua sacola está vazia
+                </StyledTitle>
+                <StyledParagraph textAlign='center'>
+                  Adicione itens
+                </StyledParagraph>
+              </div>
+            </div>
+          </dialog>
+        </StyledCartModalBox>
+      )}
+    </>
+  );
+};
 
 export default CartModal;
