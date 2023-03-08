fdc3beb5b3095fbaf1c1a45215913499dd431409
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-07 22:03:33 -0300

    feat: criando contexto para o carrinho de compras.

--- src/contexts/cartContext.tsx
+++ src/contexts/cartContext.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { createContext, useState, ReactNode } from 'react';
 
-const cartContext = () => {
-  return <div>cartContext</div>;
+interface IProduct {
+  id: number;
+  name: string;
+  category: string;
+  price: number;
+  img: string;
+}
+
+interface ICartContext {
+  cart: IProduct[];
+  setCart: React.Dispatch<React.SetStateAction<IProduct[]>>;
+}
+
+interface ICartProviderProps {
+  children: ReactNode;
+}
+
+export const CartContext = createContext({} as ICartContext);
+
+export const CartProvider = ({ children }: ICartProviderProps) => {
+  const [cart, setCart] = useState<IProduct[]>([]);
+
+  return (
+    <CartContext.Provider value={{ cart, setCart }}>
+      {children}
+    </CartContext.Provider>
+  );
 };
-
-export { cartContext };
