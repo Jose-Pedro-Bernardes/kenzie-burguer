eaa07907b32b94f0da6e4a164d063ef7a88ca8ef
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-08 00:10:54 -0300

    feat: removeAllCart, removeProduct e addToCart adicionados no contexto do Cart.

--- src/contexts/cartContext.tsx
+++ src/contexts/cartContext.tsx
@@ -1,16 +1,19 @@
 import React, { createContext, useState, ReactNode } from 'react';
 
-interface IProduct {
+export interface IProduct {
   id: number;
   name: string;
-  category: string;
-  price: number;
+  category?: string;
+  price?: number;
   img: string;
 }
 
 interface ICartContext {
   cart: IProduct[];
   setCart: React.Dispatch<React.SetStateAction<IProduct[]>>;
+  removeProduct: (id: string) => void;
+  removeAllCart: () => void;
+  addToCart: (product: IProduct) => void;
 }
 
 interface ICartProviderProps {
@@ -22,8 +25,24 @@ export const CartContext = createContext({} as ICartContext);
 export const CartProvider = ({ children }: ICartProviderProps) => {
   const [cart, setCart] = useState<IProduct[]>([]);
 
+  function addToCart(product: IProduct) {
+    if (!cart.find((producter) => producter.id === product.id)) {
+      setCart([...cart, product]);
+    }
+  }
+
+  const removeProduct = (id: string) => {
+    setCart(cart.filter((product) => product.id.toString() !== id));
+  };
+
+  const removeAllCart = () => {
+    setCart([]);
+  };
+
   return (
-    <CartContext.Provider value={{ cart, setCart }}>
+    <CartContext.Provider
+      value={{ cart, setCart, removeProduct, removeAllCart, addToCart }}
+    >
       {children}
     </CartContext.Provider>
   );
