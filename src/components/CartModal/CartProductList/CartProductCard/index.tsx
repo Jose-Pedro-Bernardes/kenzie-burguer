895d4f3cee8d2af0d5ed1ead44b03bc336611407
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-08 00:08:03 -0300

    feat: adicionando logica de remover item do carrinho.

--- src/components/CartModal/CartProductList/CartProductCard/index.tsx
+++ src/components/CartModal/CartProductList/CartProductCard/index.tsx
@@ -2,6 +2,8 @@ import { MdDelete } from 'react-icons/md';
 
 import { StyledCartProductCard } from './style';
 import { StyledTitle } from '../../../../styles/typography';
+import { useContext } from 'react';
+import { CartContext } from '../../../../contexts/CartContext';
 
 interface ICartProductCardProps {
   img: string;
@@ -9,20 +11,27 @@ interface ICartProductCardProps {
   key: string;
 }
 
-const CartProductCard = ({ img, name, key }: ICartProductCardProps) => (
-  <StyledCartProductCard key={key}>
-    <div className='imageBox'>
-      <img src={img} alt={name} />
-    </div>
-    <div className='contentBox'>
-      <StyledTitle tag='h3' $fontSize='three'>
-        {name}
-      </StyledTitle>
-      <button type='button' aria-label='Remover'>
-        <MdDelete size={24} />
-      </button>
-    </div>
-  </StyledCartProductCard>
-);
+const CartProductCard = ({ img, name, key }: ICartProductCardProps) => {
+  const { removeProduct } = useContext(CartContext);
+  return (
+    <StyledCartProductCard key={key}>
+      <div className='imageBox'>
+        <img src={img} alt={name} />
+      </div>
+      <div className='contentBox'>
+        <StyledTitle tag='h3' $fontSize='three'>
+          {name}
+        </StyledTitle>
+        <button
+          onClick={() => removeProduct(key)}
+          type='button'
+          aria-label='Remover'
+        >
+          <MdDelete size={24} />
+        </button>
+      </div>
+    </StyledCartProductCard>
+  );
+};
 
 export default CartProductCard;
