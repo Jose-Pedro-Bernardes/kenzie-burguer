a1057a1729c6cf1ee514afbcdb071d76ae60a781
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-07 21:56:19 -0300

    feat: proteção de rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,29 @@
 import { Routes, Route } from 'react-router-dom';
+import { CartProvider } from './contexts/CartContext';
+import { UserProvider } from './contexts/UserContext';
 import LoginPage from './pages/LoginPage';
+import { Protected } from './pages/Protected';
 import RegisterPage from './pages/RegisterPage';
 import ShopPage from './pages/ShopPage';
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path='/' element={<LoginPage />} />
-      <Route path='/register' element={<RegisterPage />} />
-      <Route path='/shop' element={<ShopPage />} />
-    </Routes>
+    <UserProvider>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/shop' element={<Protected />}>
+          <Route
+            index
+            element={
+              <CartProvider>
+                <ShopPage />
+              </CartProvider>
+            }
+          />
+        </Route>
+      </Routes>
+    </UserProvider>
   );
 };
 
