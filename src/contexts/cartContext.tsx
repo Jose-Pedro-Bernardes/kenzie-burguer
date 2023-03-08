7ca9639923e2c00d89d39d5696dc54cc25e128fe
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-08 11:54:46 -0300

    criando interface para as funções: showModal, openModal e closeModal.

--- src/contexts/cartContext.tsx
+++ src/contexts/cartContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, ReactNode } from 'react';
+import React, { createContext, useState } from 'react';
 
 export interface IProduct {
   id: number;
@@ -14,22 +14,34 @@ interface ICartContext {
   removeProduct: (id: string) => void;
   removeAllCart: () => void;
   addToCart: (product: IProduct) => void;
+  showModal: boolean;
+  openModal: () => void;
+  closeModal: () => void;
 }
 
 interface ICartProviderProps {
-  children: ReactNode;
+  children: React.ReactNode;
 }
 
 export const CartContext = createContext({} as ICartContext);
 
 export const CartProvider = ({ children }: ICartProviderProps) => {
   const [cart, setCart] = useState<IProduct[]>([]);
+  const [showModal, setShowModal] = useState(false);
 
   function addToCart(product: IProduct) {
     if (!cart.find((producter) => producter.id === product.id)) {
       setCart([...cart, product]);
     }
   }
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
 
   const removeProduct = (id: string) => {
     setCart(cart.filter((product) => product.id.toString() !== id));
@@ -41,7 +53,16 @@ export const CartProvider = ({ children }: ICartProviderProps) => {
 
   return (
     <CartContext.Provider
-      value={{ cart, setCart, removeProduct, removeAllCart, addToCart }}
+      value={{
+        cart,
+        setCart,
+        removeProduct,
+        removeAllCart,
+        addToCart,
+        showModal,
+        openModal,
+        closeModal,
+      }}
     >
       {children}
     </CartContext.Provider>
