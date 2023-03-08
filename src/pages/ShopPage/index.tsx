aec04179cf9852371465cd382d056f5a21741984
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-07 22:06:34 -0300

    feat: adicionando proteção de rotas na ShopPage

--- src/pages/ShopPage/index.tsx
+++ src/pages/ShopPage/index.tsx
@@ -4,17 +4,43 @@ import Header from '../../components/Header';
 import ProductList from '../../components/ProductList';
 
 import { StyledContainer } from '../../styles/grid';
+import { useContext, useEffect } from 'react';
+import api from '../../contexts/axios/axiosInstance';
+import { CartContext } from '../../contexts/CartContext';
+import { useNavigate } from 'react-router-dom';
 
-const ShopPage = () => (
-  <StyledShopPage>
-    <CartModal />
-    <Header />
-    <main>
-      <StyledContainer containerWidth={1300}>
-        <ProductList />
-      </StyledContainer>
-    </main>
-  </StyledShopPage>
-);
+const ShopPage = () => {
+  const navigate = useNavigate();
+  const { setCart } = useContext(CartContext);
+
+  useEffect(() => {
+    const token = localStorage.getItem('@KenzieBurguer:token');
+    if (token) {
+      const catchProducts = async () => {
+        try {
+          const res = await api.get('products', {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          setCart(res.data);
+        } catch (error) {
+          navigate('/');
+        }
+      };
+    }
+  });
+  return (
+    <StyledShopPage>
+      <CartModal />
+      <Header />
+      <main>
+        <StyledContainer containerWidth={1300}>
+          <ProductList />
+        </StyledContainer>
+      </main>
+    </StyledShopPage>
+  );
+};
 
 export default ShopPage;
