ca282806ee1a35a842a9dc16cd84058041c1cb63
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-07 22:05:57 -0300

    feat: adicionando pagina para lidar com a proteção de rotas.

---
+++ src/pages/Protected/index.tsx
@@ -0,0 +1,15 @@
+import React, { useContext, useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { UserContext } from '../../contexts/UserContext';
+
+export const Protected = () => {
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/');
+    }
+  });
+  return <div>{user ? <Outlet /> : null}</div>;
+};
