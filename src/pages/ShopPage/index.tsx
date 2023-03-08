653ba0c89f05bca75e7d09d8b8d92b70e83f1f84
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-08 11:58:46 -0300

    feat: adicionando container para o toast info na shop page.

--- src/pages/ShopPage/index.tsx
+++ src/pages/ShopPage/index.tsx
@@ -8,21 +8,36 @@ import { useContext, useEffect } from 'react';
 import api from '../../contexts/axios/axiosInstance';
 import { CartContext } from '../../contexts/CartContext';
 import { useNavigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 const ShopPage = () => {
   const navigate = useNavigate();
   const { cart, setCart } = useContext(CartContext);
 
   return (
-    <StyledShopPage>
-      <CartModal />
-      <Header />
-      <main>
-        <StyledContainer containerWidth={1300}>
-          <ProductList />
-        </StyledContainer>
-      </main>
-    </StyledShopPage>
+    <>
+      <StyledShopPage>
+        <CartModal />
+        <Header />
+        <main>
+          <StyledContainer containerWidth={1300}>
+            <ProductList />
+          </StyledContainer>
+        </main>
+      </StyledShopPage>
+      <ToastContainer
+        position='top-center'
+        autoClose={1500}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+      />
+    </>
   );
 };
 
