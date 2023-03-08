8cdd290f059a4f1de3e736d6faa10d2153913909
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-08 00:09:52 -0300

    feat: adicionando logica de adicionar item ao carrinho através de uma prop.

--- src/components/ProductList/ProductCard/index.tsx
+++ src/components/ProductList/ProductCard/index.tsx
@@ -1,6 +1,8 @@
 import { StyledProductCard } from './style';
 import { StyledButton } from '../../../styles/button';
 import { StyledParagraph, StyledTitle } from '../../../styles/typography';
+import { CartContext, IProduct } from '../../../contexts/CartContext';
+import { useContext } from 'react';
 
 interface IProductCardProps {
   key: string;
@@ -8,6 +10,7 @@ interface IProductCardProps {
   category: string;
   price: string;
   img: string;
+  product: IProduct;
 }
 
 const ProductCard = ({
@@ -16,22 +19,30 @@ const ProductCard = ({
   category,
   price,
   key,
-}: IProductCardProps) => (
-  <StyledProductCard key={key}>
-    <div className='imageBox'>
-      <img src={img} alt={name} />
-    </div>
-    <div className='content'>
-      <StyledTitle tag='h3' $fontSize='three'>
-        {name}
-      </StyledTitle>
-      <StyledParagraph className='category'>{category}</StyledParagraph>
-      <StyledParagraph className='price'>R$ {price}</StyledParagraph>
-      <StyledButton $buttonSize='medium' $buttonStyle='green'>
-        Adicionar
-      </StyledButton>
-    </div>
-  </StyledProductCard>
-);
+  product,
+}: IProductCardProps) => {
+  const { addToCart } = useContext(CartContext);
+  return (
+    <StyledProductCard key={key}>
+      <div className='imageBox'>
+        <img src={img} alt={name} />
+      </div>
+      <div className='content'>
+        <StyledTitle tag='h3' $fontSize='three'>
+          {name}
+        </StyledTitle>
+        <StyledParagraph className='category'>{category}</StyledParagraph>
+        <StyledParagraph className='price'>R$ {price}</StyledParagraph>
+        <StyledButton
+          onClick={() => addToCart(product)}
+          $buttonSize='medium'
+          $buttonStyle='green'
+        >
+          Adicionar
+        </StyledButton>
+      </div>
+    </StyledProductCard>
+  );
+};
 
 export default ProductCard;
